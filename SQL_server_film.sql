31d345e6023b9247b8e95f66f35a9d419f412da4
veroncav <veronika1999.v@gmail.com>
2025-01-30 11:30:46 +0200

    Update SQL_server_film.sql

--- SQL_server_film.sql
+++ SQL_server_film.sql
@@ -14,7 +14,7 @@ DROP TABLE film;
 
 --andmete sisestamine tabelisse
 INSERT INTO film(filmNimetus, aasta, eelarveHind)
-VALUES('',2024, 555555.5)
+VALUES('Barbie',2024, 555555.5)
 
 DELETE FROM film WHERE filmID=5
 
@@ -38,3 +38,31 @@ FOREIGN KEY (zanrID) REFERENCES zanr(zanrID);
 select *from film;
 select *from zanr;
 UPDATE film SET zanrID=2 WHERE filmID=3
+
+CREATE TABLE rezisoor(
+rezID int PRIMARY KEY identity(1,1),
+nimi varchar(25) NOT NULL,
+synniaeg DATE NULL,
+rikk varchar(25) NULL
+);
+
+SELECT * FROm film;
+
+--tabeli kustutamine
+DROP TABLE film;
+
+--andmete sisestamine tabelisse
+INSERT INTO rezisoor(nimi, synniaeg, riik)
+VALUES('Nolan', 1970-07-30, Britaania),
+('Mijadzaki', 1950-06-26, Jaapan),
+('Berton', 1967-07-20, Ameerika);
+
+SELECT * FROm rezisoor;
+
+CREATE TABLE kinokava(
+kinokavaID int PRIMARY KEY identity(1,1)
+rezID int ,
+film varchar(25) NOT NULL,
+seanss DATETIME NOT NULL,
+FOREIGN KEY (rezID) REFERENCES rezisoor(rezID)
+);
